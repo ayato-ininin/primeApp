36e7fb2aa0183a930f20047f80677c7f20d0c458
ayato yamauchi <>
2023-05-21 13:28:06 +0900

    Add intro func's test code

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -78,5 +79,47 @@ func Test_prompt(t *testing.T) {
 	// perform our test
 	if string(out) != "-> " {
 		t.Errorf("incorrect prompt: expeted -> but got %s", string(out))
+	}
+}
+
+func Test_intro(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set Os.Stdout to our write pipe(標準出力をバッファにリダイレクト)
+	// 基本はコンソール、ターミナルの標準出力に送られるため。
+	os.Stdout = w
+
+	intro()
+
+	// close the write pipe
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before(バッファへリダイレクトしたままだと後続のテスト等で正常に動かない)
+	os.Stdout = oldOut
+
+	// read the output of our prompt() func from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// Split output into lines
+	output := strings.Split(strings.TrimSpace(string(out)), "\n")
+	expected := []string{
+		"Is it Prime?",
+		"-----------",
+		"Enter a whole number, and we'll tell you if it's prime or not.",
+		"->",
+	}
+
+	if len(output) != len(expected) {
+		t.Errorf("Output line count mismatch: got %v want %v", len(output), len(expected))
+	}
+
+	for i, line := range output {
+		if line != expected[i] {
+			t.Errorf("Unexpected output on line %v: got %v want %v", i+1, line, expected[i])
+		}
 	}
 }
